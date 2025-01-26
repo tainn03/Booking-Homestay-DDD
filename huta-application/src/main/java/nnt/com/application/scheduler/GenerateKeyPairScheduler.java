5f694309ec342c06b1658c256000bae6096b07bd
NGUYEN NHAT TAI <tainn.dv@gmail.com>
2025-01-26 19:56:03 +0700

    chore(security): optimal code for generate key pair

--- huta-application/src/main/java/nnt/com/application/scheduler/GenerateKeyPairScheduler.java
+++ huta-application/src/main/java/nnt/com/application/scheduler/GenerateKeyPairScheduler.java
@@ -6,7 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -23,35 +25,45 @@ public class GenerateKeyPairScheduler {
     @Scheduled(cron = "0 0 0 1 * ?") // chạy vào lúc 0h ngày 1 hàng tháng
     public void generateKeyPair() {
         try {
-            Path certificatesDir = Path.of("D:/LuanVan/huta/booking/huta-start/src/main/resources/certificates");
-            if (!Files.exists(certificatesDir)) {
-                Files.createDirectories(certificatesDir);
-            }
+            generateCertificatesDir();
 
-            String command = "openssl genrsa -out D:/LuanVan/huta/booking/huta-start/src/main/resources/certificates/privatekey.pem 2048 && " +
-                    "openssl rsa -in D:/LuanVan/huta/booking/huta-start/src/main/resources/certificates/privatekey.pem -pubout -out D:/LuanVan/huta/booking/huta-start/src/main/resources/certificates/publickey.pem";
+            Process process = getProcess();
 
-            ProcessBuilder processBuilder = new ProcessBuilder(GIT_BASH_PATH, "-c", command);
-            processBuilder.redirectErrorStream(true);
-            Process process = processBuilder.start();
+            // Đọc kết quả từ command line
+            log(process);
 
-            // Đọc kết quả từ command
-            try (var reader = new java.io.BufferedReader(new java.io.InputStreamReader(process.getInputStream()))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    log.info(line);
-                }
-            }
-
-            int exitCode = process.waitFor();
-            if (exitCode == 0) {
-                log.info("Key pair generated and saved to huta-start/src/main/resources/certificates");
-            } else {
-                log.error("Key pair generation failed with exit code: {}", exitCode);
-            }
         } catch (IOException | InterruptedException e) {
             log.error("Error generating key pair", e);
             Thread.currentThread().interrupt(); // Phục hồi trạng thái interrupt
+        }
+    }
+
+    private void generateCertificatesDir() throws IOException {
+        Path certificatesDir = Path.of("D:/LuanVan/huta/booking/huta-start/src/main/resources/certificates");
+        if (!Files.exists(certificatesDir)) {
+            Files.createDirectories(certificatesDir);
+        }
+    }
+
+    private static Process getProcess() throws IOException {
+        String command = "openssl genrsa -out D:/LuanVan/huta/booking/huta-start/src/main/resources/certificates/privatekey.pem 2048 && " +
+                "openssl rsa -in D:/LuanVan/huta/booking/huta-start/src/main/resources/certificates/privatekey.pem -pubout -out D:/LuanVan/huta/booking/huta-start/src/main/resources/certificates/publickey.pem";
+
+        ProcessBuilder processBuilder = new ProcessBuilder(GIT_BASH_PATH, "-c", command);
+        processBuilder.redirectErrorStream(true);
+        return processBuilder.start();
+    }
+
+    private void log(Process process) throws IOException, InterruptedException {
+        try (var reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                log.info(line);
+            }
+        }
+
+        if (process.waitFor() == 0) {
+            log.info("KEY PAIR GENERATED AND SAVED TO huta-start/src/main/resources/certificates");
         }
     }
 }
